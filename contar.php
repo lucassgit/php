6a854841af3ff397d0cbffbca18b4a2bf9a25359
lucassgit <lucaseoviedo.92@gmail.com>
2022-07-18 20:00:03 -0300

    18/7

--- contar.php
+++ contar.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//Definicion.
+//Definicion..
 function contar($aArray){
     $contador = 0;
     foreach($aArray as $item){
