3ede5d1f5409a3e2a7833b8657e1d412320072eb
lucassgit <lucaseoviedo.92@gmail.com>
2022-07-18 22:52:28 -0300

    listo para corregir

--- clinica.php
+++ clinica.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//Pacientes
+//Pacientes..
 $aPacientes = array();
 $aPacientes[] = array(
     "dni" => "33.765.012",
