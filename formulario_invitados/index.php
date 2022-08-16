2c6aafdb6781c52eaa43036805eed46c034fedad
lucassgit <lucaseoviedo.92@gmail.com>
2022-08-16 20:52:01 -0300

    listo y funcionando

--- formulario_invitados/index.php
+++ formulario_invitados/index.php
@@ -7,7 +7,7 @@ ini_set('error_reporting', E_ALL);
 
 //Si existe el archivo invitados se carga en una variable del tipo array..
 //los DNI permitidos
-if(file_exists("invitados.txt")){
+if (file_exists("invitados.txt")) {
     $archivo = fopen("invitados.txt", "r");
     $aDocumentos = fgetcsv($archivo, 0, ",");
 } else {
@@ -15,28 +15,27 @@ if(file_exists("invitados.txt")){
     $aDocumentos = array();
 }
 
-if($_POST){
-    if(isset($_POST["btnProcesar"])){
+if ($_POST) {
+    if (isset($_POST["btnProcesar"])) {
         $documento = $_REQUEST["txtDocumento"];
 
         //Si el DNI se encuentra en la lista aparece..
-        if(in_array($documento, $aDocumentos)){
+        if (in_array($documento, $aDocumentos)) {
             $mensaje = "Bienvenido.";
         } else {
             $menaje = "No se encuentra en la lista de invitados.";
         }
     }
 
-    if(isset($_POST["btnVip"])){
+    if (isset($_POST["btnVip"])) {
         $codigo = $_REQUEST["txtCodigo"];
         //Validacion de codigo para VIP..
-        if($codigo == "verde"){
+        if ($codigo == "verde") {
             $mensaje = "Su código de acceso es " . rand(1000, 9999);
         } else {
             $mensaje = "Ud. no tiene pase VIP";
         }
     }
-
 }
 
 
@@ -44,6 +43,7 @@ if($_POST){
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -53,45 +53,48 @@ if($_POST){
     <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/estilos.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
     <title>Listado de invitados</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
+
 <body>
-	<main class="container">
-    <div class="row">
-        <div class="col-12 py-3">
-            <h1>Lista de invitados</h1>
-        </div>
-        <?php if (isset($mensaje)): ?>
-        <div class="col-12">
-            <div class="alert alert-info" role="alert">
-                <?php echo $mensaje; ?>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 py-3">
+                <h1>Lista de invitados</h1>
+            </div>
+            <?php if (isset($mensaje)) : ?>
+                <div class="col-12">
+                    <div class="alert alert-info" role="alert">
+                        <?php echo $mensaje; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+            <div class="col-12">
+                <p>Complete el siguiente formulario:</p>
+            </div>
+            <div class="col-6">
+                <form method="POST" action="">
+                    <div class="row">
+                        <div class="col-12">
+                            <p>Ingrese el DNI:
+                            <p><input type="text" name="txtDocumento" class="form-control">
+                                <input type="submit" name="btnProcesar" value="Verificar invitado" class="btn-primary">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="12-col bm-3">
+                            <p>Ingresa el código secreto para el pase VIP:
+                            <p>
+                                <input type="text" name="txtCodigo" class="form-control">
+                                <input type="submit" name="btnVip" value="Verificar código" class="btn-primary">
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
-        <?php endif;?>
-        <div class="col-12">
-            <p>Complete el siguiente formulario:</p>
-        </div>
-        <div class="col-6">
-            <form method="POST" action="">
-                <div class="row">
-                    <div class="col-12">
-                        <p>Ingrese el DNI:<p><input type="text" name="txtDocumento" class="form-control">
-                        <input type="submit" name="btnProcesar" value="Verificar invitado" class="btn-primary">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="12-col bm-3">
-                        <p>Ingresa el código secreto para el pase VIP:<p>
-                        <input type="text" name="txtCodigo" class="form-control">
-                        <input type="submit" name="btnVip" value="Verificar código" class="btn-primary">
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-	</main>
+    </main>
 </body>
+
 </html>
